084752b628cb941a5839a77030c3cdc7cc002589
VitorVieira96 <42418769+VitorVieira96@users.noreply.github.com>
2018-09-26 12:11:44 -0300

    Update Webserver.py

--- Webserver.py
+++ Webserver.py
@@ -12,10 +12,10 @@ class myHandler(BaseHTTPRequestHandler):
                 self.send_response(200)
                 self.send_header('Content-type','text/html')
                 self.end_headers()
-                self.wfile.write("<html><head><title>Trabalho 1</title></head>")
+                self.wfile.write("<html><head><title> Trabalho 1</title></head>")
                 import time
                 now=time.strftime("%c")
-                self.wfile.write("<body><p> Horario do sistema: %s</p>" %now)
+                self.wfile.write("<body><p> Data e hora do sistema: %s</p>" %now)
                 with open('/proc/uptime','r') as f:
                         uptime_seconds = float(f.readline().split()[0])
                         uptime_string = str(timedelta(seconds=uptime_seconds))
@@ -28,8 +28,9 @@ class myHandler(BaseHTTPRequestHandler):
                                 if line.strip():
                                         if line.rstrip('\n').startswith('model name'):
                                                 model_name = line.rstrip('\n').split(':')[1]
-                                                self.wfile.write("<body><p> Modelo do processador e velocidade: %s</p>" %model_name)
-						
+                                                self.wfile.write("<body><p> Modelo do processador: %s</p>" %model_name)
+
+               
                 import os
                 CPU_Pct=str(round(float(os.popen('''grep 'cpu ' /proc/stat | awk '{usage=($2+$4)*100/($2+$4+$5)} END {print usage }' ''').readline()),2))
                 self.wfile.write("<body><p>Uso do processador = %s%%</p>" %CPU_Pct)
@@ -42,8 +43,8 @@ class myHandler(BaseHTTPRequestHandler):
                         return meminfo
                 if __name__=='__main__':
                         meminfo = meminfo()
-                        self.wfile.write("<body><p>%s</p>" %'Total memory: {0}'.format(meminfo['MemTotal']))
-                        self.wfile.write("<body><p>%s</p>" %'Free memory: {0}'.format(meminfo['MemFree']))
+                        self.wfile.write("<body><p>%s</p>" %'Memoria Ram Total: {0}'.format(meminfo['MemTotal']))
+                        self.wfile.write("<body><p>%s</p>" %'Memoria Ram Livre: {0}'.format(meminfo['MemFree']))
                 def process_list():
                         pids = []
 			for subdir in os.listdir('/proc'):
@@ -51,8 +52,8 @@ class myHandler(BaseHTTPRequestHandler):
                                         pids.append(subdir)
                         return pids
                 pids = process_list()
-                self.wfile.write("<body><p>%s</p>" %'Numero Total de processos em execucao: {0}'.format(len(pids)))
-                
+                self.wfile.write("<body><p>%s</p>" %'Processos em execucao: {0}'.format(len(pids)))
+               
 try:
         server = HTTPServer(('', PORT_NUMBER), myHandler)
         print 'Started httpserver on port ' , PORT_NUMBER
