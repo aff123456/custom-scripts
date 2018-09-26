d29c312dbccc25ee87297824478482cc3bd0092d
VitorVieira96 <42418769+VitorVieira96@users.noreply.github.com>
2018-09-26 10:56:38 -0300

    Update Webserver.py

--- Webserver.py
+++ Webserver.py
@@ -12,10 +12,10 @@ class myHandler(BaseHTTPRequestHandler):
                 self.send_response(200)
                 self.send_header('Content-type','text/html')
                 self.end_headers()
-                self.wfile.write("<html><head><title>Hello World !</title></head>")
+                self.wfile.write("<html><head><title>Trabalho 1</title></head>")
                 import time
                 now=time.strftime("%c")
-                self.wfile.write("<body><p> Tempo Atual e: %s</p>" %now)
+                self.wfile.write("<body><p> Horario do sistema: %s</p>" %now)
                 with open('/proc/uptime','r') as f:
                         uptime_seconds = float(f.readline().split()[0])
                         uptime_string = str(timedelta(seconds=uptime_seconds))
@@ -28,22 +28,11 @@ class myHandler(BaseHTTPRequestHandler):
                                 if line.strip():
                                         if line.rstrip('\n').startswith('model name'):
                                                 model_name = line.rstrip('\n').split(':')[1]
-                                                self.wfile.write("<body><p> Modelo do processador: %s</p>" %model_name)
-
-                with open('/proc/cpuinfo') as f:
-                        for line in f:
-        # Ignore the blank line separating the information between
-        # details about two processing units
-                                if line.strip():
-                                        if line.rstrip('\n').startswith('flags') \
-                                                        or line.rstrip('\n').startswith('Features'):
-                                                if 'lm' in line.rstrip('\n').split():
-                                                        self.wfile.write("<body><p> Versao 64bits</p>")
-                                                else:
-                                                        self.wfile.write("<body><p> Versao 32bits</p>")
+                                                self.wfile.write("<body><p> Modelo do processador e velocidade: %s</p>" %model_name)
+						
                 import os
                 CPU_Pct=str(round(float(os.popen('''grep 'cpu ' /proc/stat | awk '{usage=($2+$4)*100/($2+$4+$5)} END {print usage }' ''').readline()),2))
-                self.wfile.write("<body><p>Porcentagem de uso do processador em porcentagem= %s</p>" %CPU_Pct)
+                self.wfile.write("<body><p>Uso do processador = %s%%</p>" %CPU_Pct)
                 def meminfo():
                         meminfo=OrderedDict()
 
@@ -63,13 +52,7 @@ class myHandler(BaseHTTPRequestHandler):
                         return pids
                 pids = process_list()
                 self.wfile.write("<body><p>%s</p>" %'Numero Total de processos em execucao: {0}'.format(len(pids)))
-                import subprocess
-                p= subprocess.Popen(["ps aux"], stdout=subprocess.PIPE, shell =True)
-                Strin=str(p.communicate()[0])
-                Strin=Strin.split('\n')
-                for id in Strin:
-                        self.wfile.write("<body><p>%s</p>" %id)
-                return
+                
 try:
         server = HTTPServer(('', PORT_NUMBER), myHandler)
         print 'Started httpserver on port ' , PORT_NUMBER
